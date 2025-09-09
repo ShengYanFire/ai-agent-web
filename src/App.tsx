cedee1c28dd81887ce8cee2025a55268f0854202
202306188@any3.com <202306188@any3.com>
2025-09-09 17:25:59 +0800

    布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import siderCollapsedSvg from './assets/sider_collapsed.svg'
+import addChatSvg from './assets/add_chat.svg'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isCollapsed, setIsCollapsed] = useState(false)
+  return <>
+    <div className='w-screen h-screen flex  bg-gray-50'>
+      <div className={`${!isCollapsed ? 'w-70' : 'w-0'} overflow-hidden text-ellipsis whitespace-nowrap transform duration-300 ease-in-out`}>
+        <div className='m-3 pt-3 mr-0'>
+          <div className='flex justify-between items-center'>
+            <img src='/ai-agent.svg' className='w-8' />
+            <img
+              src={siderCollapsedSvg}
+              className='w-6 text-gray-100  cursor-pointer rounded-lg p-1 hover:bg-gray-200'
+              onClick={() => setIsCollapsed(true)}
+            />
+          </div>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <div className='flex justify-center gap-2 border-1 border-gray-300 rounded-2xl p-2 cursor-pointer mt-3 text-gray-600 hover:bg-gray-100 hover:shadow-md transform duration-300 ease-in-out'>
+            <img
+              src={addChatSvg}
+              className='w-6 text-gray-100  cursor-pointer rounded-lg p-1'
+            />
+            新建会话
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      <div className='flex-1 bg-white rounded-lg m-3 p-3 bg-gradient-to-tr form-white to-blue-100  transform duration-300 ease-in-out'>
+        <div className='flex items-center gap-2 h-8'>
+          {isCollapsed && <>
+            <img
+              src={siderCollapsedSvg}
+              className='w-6 text-gray-100  cursor-pointer rounded-lg p-1 rotate-180 hover:bg-gray-200'
+              onClick={() => setIsCollapsed(false)}
+            />
+            <img
+              src={addChatSvg}
+              className='w-6 text-gray-100  cursor-pointer rounded-lg p-1 hover:bg-gray-200'
+              onClick={() => { }}
+            />
+          </>}
+          <div className=' max-w-100 overflow-hidden text-ellipsis whitespace-nowrap'>主要题目主要题目主要题目主要题主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目目主要题目主要题目</div>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  </>
 }
 
 export default App
