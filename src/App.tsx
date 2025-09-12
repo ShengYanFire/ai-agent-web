d91019d2e4573c316eba43d0e6fb9e444352a25e
202306188@any3.com <202306188@any3.com>
2025-09-12 14:22:35 +0800

    修改整体布局

--- src/App.tsx
+++ src/App.tsx
@@ -10,65 +10,99 @@ function App() {
   const [isCollapsed, setIsCollapsed] = useState(false)
   const { status, error, data, historyList, post, abort, initHistory, selectHistoryItem } = usePostStreamJSON<any, any>()
 
+  // 添加窗口大小变化监听
+  useEffect(() => {
+    // 定义处理窗口大小变化的函数
+    const handleResize = () => {
+      // 当窗口宽度小于1024px时自动收起侧边栏
+      if (window.innerWidth < 1024) {
+        setIsCollapsed(true)
+      } else {
+        // 当窗口宽度大于等于768px时自动展开侧边栏
+        setIsCollapsed(false)
+      }
+    }
+
+    // 初始化时执行一次
+    handleResize()
+    window.addEventListener('resize', handleResize)
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
   return <>
-    <div className='w-screen h-screen flex  bg-gray-50'>
-      <div className={`layout-transition ${!isCollapsed ? 'w-70' : 'w-0'}`}>
-        <div className='truncate m-3 pt-3 mr-0'>
-          <div className='flex justify-between items-center'>
-            <img src='/ai-agent.svg' className='w-8' />
-            <img
-              src={siderCollapsedSvg}
-              className='w-6   cursor-pointer rounded-lg p-1 hover:bg-gray-200'
-              onClick={() => setIsCollapsed(true)}
-            />
+    <div className='w-screen h-screen flex bg-gray-50'>
+      <div className={`layout-transition ${!isCollapsed ? 'w-70' : 'w-0'} absolute rounded-tr-2xl rounded-br-2xl bg-gray-50 z-1 h-full shadow-lg lg:relative`}>
+        <div className='flex flex-col p-3 pt-6 pb-6 h-full'>
+          <div className='overflow-hidden'>
+            <div className='flex justify-between items-center'>
+              <img src='/ai-agent.svg' className='w-8' />
+              <img
+                src={siderCollapsedSvg}
+                className='w-6   cursor-pointer rounded-lg p-1 hover:bg-gray-200'
+                onClick={() => setIsCollapsed(true)}
+              />
+            </div>
+
+            <div className='layout-transition flex justify-center gap-2 border-1 border-gray-300 rounded-2xl p-2 cursor-pointer mt-3 text-gray-600 hover:bg-gray-100 hover:shadow-md'>
+              <img
+                src={addChatSvg}
+                className='w-4'
+              />
+              新建会话
+            </div>
+
+            <div className='flex gap-2 items-center mt-6 mb-2 text-gray-600'>
+              <img src={historySvg} className='w-5' />
+              历史会话
+            </div>
           </div>
 
-          <div className='layout-transition flex justify-center gap-2 border-1 border-gray-300 rounded-2xl p-2 cursor-pointer mt-3 text-gray-600 hover:bg-gray-100 hover:shadow-md'>
-            <img
-              src={addChatSvg}
-              className='w-4'
-            />
-            新建会话
+          <div className='scrollbar-hide flex-1 overflow-auto'>
+            <HistoryList />
           </div>
-
-          <div className='flex gap-2 items-center mt-6 text-gray-600'>
-            <img src={historySvg} className='w-5' />
-            历史会话
-          </div>
-          <HistoryList />
         </div>
       </div>
 
-      <div className='layout-transition flex-1 flex flex-col m-3 p-3 rounded-lg bg-gradient-to-tr form-white to-blue-100'>
-        <div className='flex items-center gap-2 h-8'>
-          {isCollapsed && <>
-            <img
-              src={siderCollapsedSvg}
-              className='w-6 cursor-pointer rounded-lg p-1 rotate-180 hover:bg-gray-200'
-              onClick={() => setIsCollapsed(false)}
-            />
-            <img
-              src={addChatSvg}
-              className='w-6 cursor-pointer rounded-lg p-1 hover:bg-gray-200'
-              onClick={() => { }}
-            />
-          </>}
-          <div className='w-100 truncate'>
-            主要题目主要题目主要题目主要题主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要
-            题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目主要题目目主要
-            题目主要题目
+      <div
+        className='layout-transition flex-1 overflow-auto flex flex-col m-3 p-3 rounded-lg bg-gradient-to-tr form-white to-blue-100'
+        onClick={() => { if (window.innerWidth < 1024) setIsCollapsed(true) }}
+      >
+        <div className='flex'>
+          <div className='flex-1 flex items-center gap-2 h-8 overflow-hidden'>
+            {isCollapsed && <>
+              <img
+                src={siderCollapsedSvg}
+                className='w-6 cursor-pointer rounded-lg p-1 rotate-180 hover:bg-gray-200'
+                onClick={(e) => { e.stopPropagation(); setIsCollapsed(false) }}
+              />
+              <img
+                src={addChatSvg}
+                className='w-6 cursor-pointer rounded-lg p-1 hover:bg-gray-200'
+                onClick={() => { }}
+              />
+            </>}
+            <div className='truncate'>
+              主要题目主要题目主要题目主要题主要题目主要题目主要题主要题目主要题目主要题目主要题主要题目主要题目主要题
+            </div>
           </div>
+          <div className='flex-2'></div>
         </div>
 
-        <div className='flex-1 mt-3 mb-3 overflow-auto scrollbar-hide'>
-
-          {historyList.map((item, index) => <>
-            <div>{item.req.prompt}</div>
-            <div>{item.res.map((res, index) => res.response)}</div>
-          </>)}
+        <div className='flex-1 flex'>
+          <div className='flex-1'></div>
+          <div className='flex-6 flex flex-col'>
+            <div className='scrollbar-hide h-full overflow-auto mt-3 mb-3'>
+              {historyList.map((item, index) => <>
+                <div>{item.req.prompt}</div>
+                <div>{item.res.map((res, index) => res.response)}</div>
+              </>)}
+            </div>
+            <ChatInput post={post} status={status} abort={abort} />
+          </div>
+          <div className='flex-1'></div>
         </div>
-
-        <ChatInput post={post} status={status} abort={abort} />
       </div>
     </div>
   </>
--- src/components/HistoryList.tsx
+++ src/components/HistoryList.tsx
@@ -3,14 +3,12 @@ import { useState } from "react"
 export default () => {
     const [currentSession, setcurrentSession] = useState<any>()
     return <>
-        <div className='ml-7 text-sm text-gray-500 cursor-pointer '>
-            {[1, 2, 3, 4, 5].map(item => <div
-                key={item}
-                className={`layout-transition truncate mt-2 ${currentSession === item ? 'p-3 rounded-lg bg-gray-100 shadow-md' : ''}`}
-                onClick={() => setcurrentSession(item)}
-            >
-                {'的撒旦撒旦给三安光电撒打的撒旦撒旦给三安光电撒打'}
-            </div>)}
-        </div>
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19].map(item => <div
+            key={item}
+            className={`layout-transition ml-7 text-sm text-gray-500 cursor-pointer truncate mb-2 ${currentSession === item ? 'p-3 rounded-lg bg-gray-100 shadow-md' : ''}`}
+            onClick={() => setcurrentSession(item)}
+        >
+            {'的撒旦撒旦给三安光电撒打的撒旦撒旦给三安光电撒打'}
+        </div>)}
     </>
 }
